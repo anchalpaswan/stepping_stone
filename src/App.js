2f609acac7372b1d40ac4ca7d2939de5a73c5172
shady <anchalpaswan10@gmail.com>
2022-10-30 10:20:01 +0530

    navbar without hover effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+// import frame from "./images/Frame 1.svg"
+// import vector3 from  "./images/Vector 3.svg";
+// import card from "./images/card.svg";
+// import card4 from "./images/card4.svg";
+// import card3 from "./images/card3.svg";
+// import card2 from "./images/card2.svg"
+import TopBar from "./components/topbar/TopBar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    // <div className="main">
+    //   {/* <img className="vector-3" src={vector3} alt="Vector 3"/>
+    //   <img className="vector-3" src={frame} alt="Vector 3"/> */}
+
+    //   <div className="nav">
+        
+    //   </div>
+    //   <div className="card-container">
+    //     <img className="card" src={card} alt="Vector 3"/>
+    //     <img className="card" src={card2} alt="Vector 3"/>
+    //     <img className="card" src={card3} alt="Vector 3"/>
+    //     <img className="card" src={card4} alt="Vector 3"/>
+    //  </div>
+    //   </div>
+    <>
+    <div className="main">
+
+    <TopBar/>
+
     </div>
+    </>
   );
 }
 
 export default App;
+
+
+//create pages :home server add course login /register  
+  
