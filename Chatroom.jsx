1b460ad2089cb3544091a57044fbc5ca8fbca5cb
Anchal <49560555+anchalpaswan@users.noreply.github.com>
2022-10-31 11:41:27 +0530

    Add files via upload

---
+++ Chatroom.jsx
@@ -0,0 +1,14 @@
+import SlideBar from "../../components/slidebar/SlideBar";
+import "./chatroom.css"
+
+import React from 'react'
+
+export default function Chatroom() {
+  return (
+    <><div className="chatroom">
+        <SlideBar/>
+
+    </div>
+    </>
+  )
+}
---
+++ Home.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+// import TopBar from '../../components/topbar/Topbar';
+export default function Home(){
+    return (
+
+    <>
+
+    </>
+    );
+
+}
